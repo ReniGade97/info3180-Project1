630cf7b607edeb5c66d8e01f789fe533f228d6c3
Renee Samms <renee.samms@mymona.uwi.edu>
2019-03-19 04:03:07 +0000

    profiles edit

--- app/models.py
+++ app/models.py
@@ -18,13 +18,15 @@ class UserProfile(db.Model):
     photo = db.Column(db.String(300))
     created_on = db.Column(db.DateTime())
     
-    def __init__(self, first_name, last_name, gender, email, location, biography):
+    def __init__(self, first_name, last_name, gender, email, location, biography, photo, created_on):
         self.first_name = first_name
         self.last_name  = last_name
         self.gender     = gender
         self.email      = email
         self.location   = location
         self.biography  = biography
+        self.photo      = photo
+        self.created_on = created_on
         
     def is_authenticated(self):
         return True
